96080d977290bf6c0ab09633d8880089cc46a96e
Evan Peterson <evanpeterson17@gmail.com>
2019-02-25 12:54:05 -0700

    Add analysis and plots for dates and contributor ranges

--- json-log-processor.py
+++ json-log-processor.py
@@ -4,10 +4,18 @@ import json
 import re
 from datetime import datetime
 
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
 
 def parsedate(datestr):
-    # Example of the .json format: 2019-02-14 16:02:18
+    """Parse json date string to datetime"""
     return datetime.strptime(datestr, "%Y-%m-%d %X")
+
+
+def string2date(datestr):
+    """Parse json date string to matplotlib.date"""
+    return mdates.date2num(parsedate(datestr))
 
 
 def safe_add_to_key(dct, key, value, default=0):
@@ -72,8 +80,54 @@ def find_top_contributors(commits, topn, topperct):
     return contrib_list
 
 
-def analyze_dates(commits):
-    pass
+def analyze_dates(commits, filename, minyear=2004, maxyear=2019):
+    years = mdates.YearLocator()  # every year
+    months = mdates.MonthLocator()  # every month
+    yearsFmt = mdates.DateFormatter('%Y')
+    # Grab the date of each commit. Filter out bad data
+    print("Extracting dates...")
+    pltdates = [
+        string2date(commit["date"]) for commit in commits
+        if int(commit["date"][0:4]) >= minyear
+        and int(commit["date"][0:4]) <= maxyear
+    ]
+    # Plot it out. Source: https://matplotlib.org/examples/api/date_demo.html
+    print("Plotting dates...")
+    fig, ax = plt.subplots()
+    # Have a histogram bar for each quarter of data.
+    ax.hist(pltdates, color="green", bins=((maxyear - minyear + 1) * 4))
+    # Format the ticks
+    ax.xaxis.set_major_locator(years)
+    ax.xaxis.set_major_formatter(yearsFmt)
+    ax.xaxis.set_minor_locator(months)
+    ax.format_xdata = mdates.DateFormatter('%Y-%m-%d')
+    fig.autofmt_xdate()
+    plt.savefig("./reports/{}".format(filename))
+    print("Finished.")
+
+
+def find_contributor_ranges(commits, filename):
+    contrib_sets = {}
+    # Track which years each contributor contributed in.
+    print("Tracking contribution years...")
+    for commit in commits:
+        commit_year = int(commit["date"][0:4])
+        for person in commit["signedOffBy"]:
+            if person not in contrib_sets:
+                contrib_sets[person] = set([])
+            contrib_sets[person].add(commit_year)
+    print("Aggregating contribution years...")
+    contrib_cnts = [len(s) for s in contrib_sets.values()]
+    print("Plotting...")
+    fig, ax = plt.subplots()
+    max_cnt = max(contrib_cnts)
+    ax.hist(contrib_cnts, bins=max_cnt, density=True, stacked=True)
+    plt.xticks(range(1, max_cnt + 1))
+    plt.xlabel("Number of years contributed in")
+    plt.ylabel("Portion of the whole")
+    # Format the ticks
+    plt.savefig("./reports/{}".format(filename))
+    print("Finished.")
 
 
 def main(filename, topn):
@@ -82,8 +136,10 @@ def main(filename, topn):
     with open(filename, "r") as fp:
         commits = json.load(fp)
     print("Finished loading file.")
-    contrib_list = find_top_contributors(commits, topn, .8)
+    # contrib_list = find_top_contributors(commits, topn, .8)
     # find_top_email_domians(contrib_list, topn)
+    # analyze_dates(commits, "commit-hist.png")
+    find_contributor_ranges(commits, "longevity-hist.png")
 
 
 if __name__ == '__main__':
